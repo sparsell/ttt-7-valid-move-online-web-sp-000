db1808ad897caf125c3037c8352146ed0beaaaca
Sonja Parsell <hello@sparsell.com>
2020-03-11 12:23:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 
-def position_taken?(board, index)
-     !(board[index] == " " || board[index] == "" || board[index] == nil)
-end
+#def position_taken?(board, index)
+#     !(board[index] == " " || board[index] == "" || board[index] == nil)
+#end
 
 def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
