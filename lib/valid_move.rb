ec008640ea7ae35d807b2dcf5e51f62f6a399d33
Sonja Parsell <hello@sparsell.com>
2020-03-11 12:21:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,6 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.to_i - 1
   index.between?(0, 8) && !position_taken?(board, index)
   if position_taken?(board, index) == true
     ""
